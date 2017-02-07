b7e6b2003039448aba66d17ed40507ad976f72d9
Ishi Ruy <dev@nhz.io>
2017-02-07 03:42:11 +0200

    added tests

--- test.js
+++ test.js
@@ -1,11 +1,67 @@
 import test from 'ava'
+import {EventEmitter} from 'events'
+import init from '.'
 
-import capture from '.'
+test('capture nothing', t => {
+    const $capture = init(() => t.fail())
+    t.is($capture(undefined), undefined) // eslint-disable-line no-undefined
+    t.is($capture(null), null)
+    t.is($capture(''), '')
+    t.is($capture(0), 0)
+})
 
-const {init, prepare} = capture
+test('capture error', t => {
+    t.plan(1)
 
-test.todo('capture')
+    const pass = new Error()
+    const $capture = init(err => t.is(err, pass))
+    
+    $capture(pass)
+})
 
-test.todo('init')
+test('wrap function and capture thrown errors', t => {
+    t.plan(2)
 
-test.todo('prepare')
+    const pass = new Error()
+    const $capture = init(err => t.is(err, pass))
+    
+    try {
+        $capture(() => {
+            throw pass
+        })()
+    }
+    catch (err) {
+        t.is(err, pass)
+    }
+})
+
+test('wrap function and capture callback errors', t => {
+    t.plan(2)
+
+    const pass = new Error()
+    const $capture = init(err => t.is(err, pass))
+
+    $capture(() => {
+        t.pass()
+    })(pass)
+})
+
+test('wrap promise and capture rejected errors', t => {
+    t.plan(2)
+
+    const pass = new Error()
+    const $capture = init(err => t.is(err, pass))
+
+    return $capture(Promise.reject(pass)).catch(err => t.is(err, pass))
+})
+
+test.cb('wrap event emitter and capture emitted errors', t => {
+    const pass = new Error()
+    const $capture = init(err => {
+        t.is(err, pass)
+        t.end()
+    })
+
+    $capture(new EventEmitter()).emit('error', pass)
+
+})
